fd690cef0d131bc13fd142e2200c5b08ae8f6705
Marco Rodriguez-Salinas <marco.rodriguez113@gmail.com>
2023-03-28 14:46:40 -0700

    Restarting way of thinking

--- suduku.go
+++ suduku.go
@@ -1,32 +1,42 @@
 package main
 
 import (
-	// "crypto/rand"
 	"fmt"
 	"math/big"
 	"math/rand"
-	"strings"
+	// "strings"
 
 	"golang.org/x/exp/slices"
 )
-
-type Board struct {
-	game_board []int
+type Cell struct {
+	val int
+	allowed []int
+	grid int
 }
 
-var suduku = Board{
-	[]int{0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0},
+type Board struct {
+	game_board [][]Cell
 }
 
 func main() {
+	const N = 9
+	var Sudoku = Board{make([][]Cell, N)}
+	gridNum := 1
+
+	// init board to all 0's & a complete allow list, with proper grid number
+	for i := 0; i < N; i++ {
+		if (i+1) % 3 == 1 {
+			gridNum=i+1
+		}
+		for j := 1; j <= N; j++ {
+			Sudoku.game_board[i] = append(Sudoku.game_board[i], Cell{0, []int{1,2,3,4,5,6,7,8,9}, gridNum})
+			if j % 3 == 0 {
+				gridNum++
+			}
+		}
+		gridNum = gridNum - 3
+	}
+
 	drand := "76a48fc2dce4bada35518640a0e414d2cf4566b3e7a1b8a5071b70eb5be34444"
 	bi := new(big.Int)
 	bi.SetString(drand, 16)
@@ -40,29 +50,26 @@ func main() {
 	fmt.Println("psuedo-random: ", randNum)
 	
 	// Pass in the r1 seed.
-	suduku.fillBoard(0, randNum, []int{1,2,3,4,5,6,7,8,9})
-	suduku.printBoard()
+	// Sudoku.fillBoard(0, randNum, []int{1,2,3,4,5,6,7,8,9})
+	Sudoku.printBoard()
 }
 
 // This function will pretty print the board,
 // Written very specifically for a 1D board layout
 func (b *Board) printBoard() {
 	fmt.Println("| --------------------- |")
-	fmt.Print("| ")
-	for idx, val := range b.game_board {
-		fmt.Print(val, " ")
-		if (idx+1)%3 == 0 {
-			fmt.Print("| ")
-		}
-		if (idx+1)%9 == 0 {
-			fmt.Println("")
-			fmt.Print("| ")
-		}
-		if (idx+1)%27 == 0 {
-			fmt.Println("--------------------- |")
-			if !((idx+1)%81 == 0) {
+	for idx, row := range b.game_board {
+		fmt.Print("| ")
+		for jdx, cell := range row {
+			fmt.Print(cell.val, " ")
+			
+			if (jdx+1) % 3 == 0 {
 				fmt.Print("| ")
 			}
+		}
+		fmt.Println()
+		if (idx+1) % 3 == 0 {
+			fmt.Println("| --------------------- |")
 		}
 	}
 }
@@ -70,49 +77,22 @@ func (b *Board) printBoard() {
 // This recursive function will fill the whole grid
 // Parameters: starting index, rolling index, array
 
-// pass in an array of allowed values respective to every square, this would avoid the need for a "tries" var 
+// pass in an array of allowed values respective to every square
 // && looping to check for every row & col. For this to work I need a way to reset values after every row, column & grid.
 
 // Pass in the seed value so I can keep getting random values from it
-// For this to work, i neeed to know when I have tried every value at least once & backtrack.
+// For this to work, i need remove the currently tried value from the allow list
 func (b *Board) fillBoard(ri int, val int, allowed []int) {
-	// Closing condition should be:
-	// if every Col,Row,&Grid are solved
-	// Or I reached the end of the board??
-	if ri >= len(b.game_board) {
-		return
-	}
-	if val > 9 {
-		val = 1
-	}
-	// For loop necessary for backtracking
-	// for _, val := range b.game_board {
-		b.game_board[ri] = val
-	// if bad row, try different value
-	
-	// if bad col, try different value
 
-	// if bad grid, try different value
-
-		// if good, continue
-		b.fillBoard(ri+1, val+1, remove(allowed, b.game_board[ri]))
-	// }
-	// 1. Place a number then check.
-	// 2. If number has already been placed in the last 9*floor(ri/9). Then try a different number.
-	// 3. Backtrack if number placed has been seen in Col before.
-	// if number has been placed in last i - 9 places; for i > 0. Then try different number.
-	// 4. Backtrack if number placed has been seen in 3x3 Grid
-	// Use array method. i will give me their Grid location
+// 	// 1. Place a number then check.
+// 	// 2. If number has already been placed in the last 9*floor(ri/9). Then try a different number.
+// 	// 3. Backtrack if number placed has been seen in Col before.
+// 	// if number has been placed in last i - 9 places; for i > 0. Then try different number.
+// 	// 4. Backtrack if number placed has been seen in 3x3 Grid
+// 	// Use array method. i will give me their Grid location
 }
 
-func find(arr[]int, key int) (int, bool) {
-	for i := range arr {
-		if arr[i] == key {
-			return i, true
-		}
-	}
-	return -1, false
-}
+// This function removes an element at specific index from array
 func remove(arr []int, key int) []int {
 	if i, exists := find(arr, key); exists {
 		return slices.Delete(arr, i, i+1)
@@ -120,60 +100,12 @@ func remove(arr []int, key int) []int {
 	return arr
 }
 
-// This function will do a final check of the whole grid
-// Return True if complete and False if there is a mistake
-func (b *Board) checkFin() bool {
-	char := []string{"R", "C", "G"}
-	for i := 0; i < 3; i++ {
-		for j := 1; j <= 9; j++ {
-			if _, valid := b.check([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, b.createSubArray(char[i], j)); !valid {
-				return false
-			}
+// This function finds and returns the index of an element else returns -1 & false
+func find(arr[]int, key int) (int, bool) {
+	for i := range arr {
+		if arr[i] == key {
+			return i, true
 		}
 	}
-	return true
-}
-
-// This function checks to see if a row contains the same number more than once
-// Parameters: array of numbers to check
-func (b *Board) check(history[]int, row []int) ([]int, bool) {
-	for _, val := range row {
-		if val == 0 {
-			return history, false
-		}
-		if history[val-1] == 0 {
-			return history, false
-		}
-		if val == history[val-1] {
-			history[val-1] = 0
-		}
-	}
-	return history, true
-}
-
-// This function will create sub-array out of desired selection
-// options: G=Grid, R=Row, C=Col. + #1-9 to choose either Grid, Row, or Col
-func (b *Board) createSubArray(char string, num int) []int {
-	char = strings.ToLower(char)
-	if num <= 0 || num >= 10 {
-		fmt.Printf("num out of bounds")
-	}
-	switch char {
-	case "r":
-		return b.game_board[(num-1)*9 : ((num-1)*9)+9]
-	case "c":
-		tempArr := []int{}
-		for idx := num - 1; idx < len(b.game_board); idx += 9 {
-			tempArr = append(tempArr, b.game_board[idx])
-		}
-		return tempArr
-	case "g":
-		tempArr := []int{}
-		m := map[int]int{1: 0, 2: 3, 3: 6, 4: 27, 5: 30, 6: 33, 7: 54, 8: 57, 9: 60}
-		for idx := m[num]; len(tempArr) < 9; idx += 9 {
-			tempArr = append(tempArr, b.game_board[idx], b.game_board[idx+1], b.game_board[idx+2])
-		}
-		return tempArr
-	}
-	return []int{0}
+	return -1, false
 }
--- suduku_test.go
+++ suduku_test.go
@@ -1,181 +1,48 @@
 package main
 
 import (
+	"os"
 	"reflect"
 	"testing"
 )
 
-var test_board = Board{
-	[]int{1, 7, 9, 9, 5, 4, 2, 3, 6,
-		3, 6, 1, 2, 7, 9, 8, 5, 4,
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 3, 0, 0, 7, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 5, 0, 0, 0, 7, 0, 0,
-		9, 0, 1, 2, 3, 4, 5, 0, 0,
-		0, 2, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 9},
+var complete_board = Board{[][]Cell{
+		[]Cell{ {1,[]int{},1}, {2,[]int{},1}, {3,[]int{},1}, {6,[]int{},2}, {7,[]int{},2}, {8,[]int{},2}, {9,[]int{},3}, {4,[]int{},3}, {5,[]int{},3}},
+		[]Cell{ {5,[]int{},1}, {8,[]int{},1}, {4,[]int{},1}, {2,[]int{},2}, {3,[]int{},2}, {9,[]int{},2}, {7,[]int{},3}, {6,[]int{},3}, {1,[]int{},3}},
+		[]Cell{ {9,[]int{},1}, {6,[]int{},1}, {7,[]int{},1}, {1,[]int{},2}, {4,[]int{},2}, {5,[]int{},2}, {3,[]int{},3}, {2,[]int{},3}, {8,[]int{},3}},
+		[]Cell{ {3,[]int{},4}, {7,[]int{},4}, {2,[]int{},4}, {4,[]int{},5}, {6,[]int{},5}, {1,[]int{},5}, {5,[]int{},6}, {8,[]int{},6}, {9,[]int{},6}},
+		[]Cell{ {6,[]int{},4}, {9,[]int{},4}, {1,[]int{},4}, {5,[]int{},5}, {8,[]int{},5}, {3,[]int{},5}, {2,[]int{},6}, {7,[]int{},6}, {4,[]int{},6}},
+		[]Cell{ {4,[]int{},4}, {5,[]int{},4}, {8,[]int{},4}, {7,[]int{},5}, {9,[]int{},5}, {2,[]int{},5}, {6,[]int{},6}, {1,[]int{},6}, {3,[]int{},6}},
+		[]Cell{ {8,[]int{},7}, {3,[]int{},7}, {6,[]int{},7}, {9,[]int{},8}, {2,[]int{},8}, {4,[]int{},8}, {1,[]int{},9}, {5,[]int{},9}, {7,[]int{},9}},
+		[]Cell{ {2,[]int{},7}, {1,[]int{},7}, {9,[]int{},7}, {8,[]int{},8}, {5,[]int{},8}, {7,[]int{},8}, {4,[]int{},9}, {3,[]int{},9}, {6,[]int{},9}},
+		[]Cell{ {7,[]int{},7}, {4,[]int{},7}, {5,[]int{},7}, {3,[]int{},8}, {1,[]int{},8}, {6,[]int{},8}, {8,[]int{},9}, {9,[]int{},9}, {2,[]int{},9}},
+	},
 }
 
-var complete_board = Board{
-	[]int{
-		1, 2, 3, 6, 7, 8, 9, 4, 5,
-		5, 8, 4, 2, 3, 9, 7, 6, 1,
-		9, 6, 7, 1, 4, 5, 3, 2, 8,
-		3, 7, 2, 4, 6, 1, 5, 8, 9,
-		6, 9, 1, 5, 8, 3, 2, 7, 4,
-		4, 5, 8, 7, 9, 2, 6, 1, 3,
-		8, 3, 6, 9, 2, 4, 1, 5, 7,
-		2, 1, 9, 8, 5, 7, 4, 3, 6,
-		7, 4, 5, 3, 1, 6, 8, 9, 2},
+const N = 9
+var test_board = Board{make([][]Cell, N)}
+
+func TestMain(m *testing.M){
+	gridNum := 1
+		// init board to all 0's & a complete allow list, with proper grid number
+		for i := 0; i < N; i++ {
+			if (i+1) % 3 == 1 {
+				gridNum=i+1
+			}
+			for j := 1; j <= N; j++ {
+				test_board.game_board[i] = append(test_board.game_board[i], Cell{0, []int{1,2,3,4,5,6,7,8,9}, gridNum})
+				if j % 3 == 0 {
+					gridNum++
+				}
+			}
+			gridNum = gridNum - 3
+		}
+		os.Exit(m.Run())
 }
 
 func TestPrint(t *testing.T) {
 	t.Run("PrintBoard", func(t *testing.T) {
 		test_board.printBoard()
-	})
-}
-
-func TestCreateSubRow(t *testing.T) {
-	var tests = []struct {
-		name string
-		char string
-		val  int
-		want []int
-	}{
-		{`First_Row`, "r", 1, test_board.game_board[0:9]},
-		{`Second_Row`, "r", 2, test_board.game_board[9:18]},
-		{`Third_Row`, "r", 3, test_board.game_board[18:27]},
-		{`Fourth_Row`, "r", 4, test_board.game_board[27:36]},
-		{`Fifth_Row`, "r", 5, test_board.game_board[36:45]},
-		{`Sixth_Row`, "r", 6, test_board.game_board[45:54]},
-		{`Seventh_Row`, "r", 7, test_board.game_board[54:63]},
-		{`Eighth_Row`, "r", 8, test_board.game_board[63:72]},
-		{`Ninth_Row`, "r", 9, test_board.game_board[72:81]},
-	}
-	for _, tt := range tests {
-		t.Run("Create Sub Rows", func(t *testing.T) {
-			result := test_board.createSubArray(tt.char, tt.val)
-			if len(result) != len(tt.want) {
-				t.Errorf("Slices are not equal size.. want:%d res:%d", len(tt.want), len(result))
-			}
-			if !reflect.DeepEqual(result, tt.want) {
-				t.Fatal("Slices not the same.. want: ", tt.want, "res: ", result)
-			}
-		})
-	}
-}
-
-func TestCreateSubCol(t *testing.T) {
-	var tests = []struct {
-		name string
-		char string
-		val  int
-		want []int
-	}{
-		{`First_Col`, "c", 1, []int{1, 3, 0, 0, 0, 0, 9, 0, 0}},
-		{`Second_Col`, "c", 2, []int{7, 6, 0, 3, 0, 0, 0, 2, 0}},
-		{`Third_Col`, "c", 3, []int{9, 1, 0, 0, 0, 5, 1, 0, 0}},
-		{`Fourth_Col`, "c", 4, []int{9, 2, 0, 0, 0, 0, 2, 0, 0}},
-		{`Fifth_Col`, "c", 5, []int{5, 7, 0, 7, 0, 0, 3, 0, 0}},
-		{`Sixth_Col`, "c", 6, []int{4, 9, 0, 0, 0, 0, 4, 0, 0}},
-		{`Seventh_Col`, "c", 7, []int{2, 8, 0, 0, 0, 7, 5, 0, 0}},
-		{`Eighth_Col`, "c", 8, []int{3, 5, 0, 0, 0, 0, 0, 0, 0}},
-		{`Ninth_Col`, "c", 9, []int{6, 4, 0, 0, 0, 0, 0, 0, 9}},
-	}
-	for _, tt := range tests {
-		t.Run("Create Sub Cols", func(t *testing.T) {
-			result := test_board.createSubArray(tt.char, tt.val)
-			if len(result) != len(tt.want) {
-				t.Errorf("Slices are not equal size.. want:%d res:%d", len(tt.want), len(result))
-			}
-			if !reflect.DeepEqual(result, tt.want) {
-				t.Fatal("Slices not the same.. want: ", tt.want, "res: ", result)
-			}
-		})
-	}
-}
-
-func TestCreateSubGrid(t *testing.T) {
-	var tests = []struct {
-		name string
-		char string
-		val  int
-		want []int
-	}{
-		{`First_Grid`, "g", 1, []int{1, 7, 9, 3, 6, 1, 0, 0, 0}},
-		{`Second_Grid`, "g", 2, []int{9, 5, 4, 2, 7, 9, 0, 0, 0}},
-		{`Third_Grid`, "g", 3, []int{2, 3, 6, 8, 5, 4, 0, 0, 0}},
-		{`Fourth_Grid`, "g", 4, []int{0, 3, 0, 0, 0, 0, 0, 0, 5}},
-		{`Fifth_Grid`, "g", 5, []int{0, 7, 0, 0, 0, 0, 0, 0, 0}},
-		{`Sixth_Grid`, "g", 6, []int{0, 0, 0, 0, 0, 0, 7, 0, 0}},
-		{`Seventh_Grid`, "g", 7, []int{9, 0, 1, 0, 2, 0, 0, 0, 0}},
-		{`Eighth_Grid`, "g", 8, []int{2, 3, 4, 0, 0, 0, 0, 0, 0}},
-		{`Ninth_Grid`, "g", 9, []int{5, 0, 0, 0, 0, 0, 0, 0, 9}},
-	}
-	for _, tt := range tests {
-		t.Run("Create Sub Grids", func(t *testing.T) {
-			result := test_board.createSubArray(tt.char, tt.val)
-			if len(result) != len(tt.want) {
-				t.Errorf("Slices are not equal size.. want:%d res:%d", len(tt.want), len(result))
-			}
-			if !reflect.DeepEqual(result, tt.want) {
-				t.Fatal("Slices not the same.. want: ", tt.want, "res: ", result)
-			}
-		})
-	}
-}
-
-func TestRowCheck(t *testing.T) {
-	var tests = []struct {
-		name string
-		row  []int
-		want bool
-	}{
-		{"Check correct", []int{1, 7, 9, 8, 5, 4, 2, 3, 6}, true},
-		{"Check duplicate number", []int{1, 7, 7, 8, 5, 4, 2, 6, 6}, false},
-		{"Check one zero", []int{1, 7, 0, 8, 5, 4, 2, 9, 6}, false},
-		{"Check multiple zeros", []int{1, 7, 0, 8, 5, 4, 2, 0, 6}, false},
-		{"Check all zeros", []int{0, 0, 0, 0, 0, 0, 0, 0, 0}, false},
-	}
-	for _, tt := range tests {
-		t.Run("Check Sub Row", func(t *testing.T) {
-			_, result := test_board.check([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, tt.row)
-			if result != tt.want {
-				t.Error("Repeat number detected in row", tt.row)
-			}
-		})
-	}
-}
-
-// not tested thoroughly
-func TestColCheck(t *testing.T) {
-	var tests = []struct {
-		name string
-		col  []int
-		want bool
-	}{
-		{"Check correct", []int{1, 7, 9, 8, 5, 4, 2, 3, 6}, true},
-		// {"Check duplicate number", []int{1, 7, 7, 8, 5, 4, 2, 6, 6}, false},
-		// {"Check one zero", []int{1, 7, 0, 8, 5, 4, 2, 9, 6}, false},
-		// {"Check multiple zeros", []int{1, 7, 0, 8, 5, 4, 2, 0, 6}, false},
-		// {"Check all zeros", []int{0, 0, 0, 0, 0, 0, 0, 0, 0}, false},
-	}
-	for _, tt := range tests {
-		t.Run("Check Sub Col", func(t *testing.T) {
-			_, result := test_board.check([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, tt.col)
-			if result != tt.want {
-				t.Error("Repeat number detected in Col", tt.col)
-			}
-		})
-	}
-}
-
-func TestCheckFin(t *testing.T) {
-	t.Run("Check Finale", func(t *testing.T) {
-		if !complete_board.checkFin() {
-			t.Error("Board is incomplete")
-		}
 	})
 }
 
